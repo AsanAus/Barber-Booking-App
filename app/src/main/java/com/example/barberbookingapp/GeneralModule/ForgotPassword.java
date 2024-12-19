cb2431c5462c014da9ad4bd89b151470cdb70c16
BigBoss2403 <hassanfirdaus00@gmail.com>
2024-12-19 22:29:15 +0800

    push General Module frontend

--- app/src/main/java/com/example/barberbookingapp/GeneralModule/ForgotPassword.java
+++ app/src/main/java/com/example/barberbookingapp/GeneralModule/ForgotPassword.java
@@ -1,8 +1,6 @@
-package com.example.barberbookingapp;
+package com.example.barberbookingapp.GeneralModule;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,33 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.barberbookingapp.GeneralModule.Splashing;
+import com.example.barberbookingapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotPassword extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        // Delay for 2 seconds, then launch the MainActivity
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, Splashing.class);
-                startActivity(intent);
-                finish(); // Close Splashing activity so it doesn't come back
-            }
-        }, 2000); // 2000ms = 2 seconds
-
-
+        setContentView(R.layout.activity_forgot_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
-
         });
     }
+
 }
--- app/src/main/java/com/example/barberbookingapp/GeneralModule/Splashing.java
+++ app/src/main/java/com/example/barberbookingapp/GeneralModule/Splashing.java
@@ -1,4 +1,4 @@
-package com.example.barberbookingapp;
+package com.example.barberbookingapp.GeneralModule;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,33 +10,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.barberbookingapp.GeneralModule.Splashing;
+import com.example.barberbookingapp.MainActivity;
+import com.example.barberbookingapp.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Splashing extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashing_screen);
 
         // Delay for 2 seconds, then launch the MainActivity
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, Splashing.class);
+                Intent intent = new Intent(Splashing.this, Login.class);
                 startActivity(intent);
                 finish(); // Close Splashing activity so it doesn't come back
             }
         }, 2000); // 2000ms = 2 seconds
 
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
-
         });
     }
 }
